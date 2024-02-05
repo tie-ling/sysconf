2c6092d59deaaa021a87542e75b83a6623ee96ac
Yan Cao <yan@example.com>
2024-02-05 15:39:47 +0100

    fix name

--- imports/services/server.nix
+++ imports/services/server.nix
@@ -18,7 +18,7 @@
       };
     };
     # nfs4 does not need rpcbind
-    services.rpcbind.enable = lib.mkForce false;
+    rpcbind.enable = lib.mkForce false;
     nfs = {
       # kodi/coreelec uses nfs3 by default
       # switch to nfs4 by using settings here
